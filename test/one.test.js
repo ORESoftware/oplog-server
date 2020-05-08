de8f3f2f1a25802528a0f99a3c040cec99046f13
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-05-07 20:09:14 -0700

    squash-me

--- test/one.test.js
+++ test/one.test.js
@@ -2,3 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var CDTOplogServer = require("../dist");
 console.log('CDTOplogServer exports:', CDTOplogServer);
+var rxjs_1 = require("rxjs");
+var s = new rxjs_1.Subject();
+var sub = s.subscribe(function (v) { });
+var v = [];
+v.push(sub, sub, sub);
